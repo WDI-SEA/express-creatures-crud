5fb753eff76e9d994ec5d2eeb8c62344d35a1ca9
hamazumi <khamamoto12@gmail.com>
2021-05-28 15:49:41 -0700

    finish data API

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ app.get('/', (req, res) => {
     res.json({ msg: 'hello dinos!'})
 })
 
+
+
 //GET /dinosaurs -- READ all dinos
 app.get('/dinosaurs', (req, res) => {
     //read the dino file
@@ -43,14 +45,60 @@ app.post('/dinosaurs', (req, res) => {
 })
 
 //GET /dinosaurs/new -- READ (show) a form to add dino
+app.get('/dinosaurs/new', (req, res) => {
+    res.json({ msg: 'show form to add a dino'})
+})
 
 //GET /dinosaurs/:id -- READ one specific dino
+app.get('/dinosaurs/:id', (req, res) => {
+    //get our dino data
+    const dinosaurs = fs.readFileSync('./dinosaurs.json')
+    const dinoData = JSON.parse(dinosaurs)
+
+    //look up one dino with the request parameters
+    const dino = dinoData[req.params.id]
+
+    //send one dino back
+    res.json({dino})
+})
  
 //GET /dinosaurs/edit:id -- READ (show) form to edit one dino
+app.get('/dinosaurs/edit/:id', (req, res) => {
+    res.json({ msg: `show form to edit ${req.params.id}`})
+})
 
 //PUT /dinosaurs/:id -- UPDATE (edit) one dino -- redirect to /dinosaur
+app.put('/dinosaurs/:id', (req, res) => {
+    //get dino data from our json
+    const dinosaurs = fs.readFileSync('./dinosaurs.json')
+    const dinoData  = JSON.parse(dinosaurs)
+
+    //find on dino from the req.params.id and use the req body to update
+    dinoData[req.params.id].name = req.body.name
+    dinoData[req.params.id].type = req.body.type
+
+    //write the json file
+    fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinoData))
+
+    //rediect to /dinosaurs
+    res.redirect('/dinosaurs')
+})
 
 //DELETE /dinosaurs/:id -- DESTROY one specific dino
+app.delete('/dinosaurs/:id', (req, res) => {
+    // get our dino json
+    const dinosaurs = fs.readFileSync('./dinosaurs.json')
+    const dinoData = JSON.parse(dinosaurs)
+
+    //remove one dino from the array
+    dinoData.splice(req.params.id, 1)
+
+    //save dinosaurs.json
+    fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinoData))
+
+    //redirect to /dinosaurs
+    res.redirect('/dinosaurs')
+})
 
 //Listen on PORT
 app.listen(PORT, ()=> {
