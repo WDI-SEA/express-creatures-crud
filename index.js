5109e09180fd5e32be46c2e5fecef6ef6430e2d4
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-05-28 16:33:32 -0500

    Added dinosaurs.json and parsed.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const rowdy = require("rowdy-logger")
+const fs = require("fs")
 
 const app = express()
 const rowdyResults = rowdy.begin(app)
@@ -7,9 +8,35 @@ const rowdyResults = rowdy.begin(app)
 const PORT = 7000
 const log = console.log
 
+// ROUTES
 app.get("/", (req, res) => {
     res.json({ msg: "Hello dinos!"})
 })
+
+// GET /dinosaurs -- READ (list) all dinos
+app.get("/dinosaurs", (req, res) => {
+    // read dinosaurs.json
+    const dinosaurs = fs.readFileSync("./dinosaurs.json")
+    // parse json buffer
+    const dinoData = JSON.parse(dinosaurs)
+    // send back the json
+    res.json({dinoData})
+
+
+    // send back the json
+})
+
+// POST /dinosaurs -- CREATE a new dino -- redirect to /dinosaurs
+
+// GET /dinosaurs/new -- READ (show) a form to add a dino
+
+// GET /dinosaurs/:id -- READ (list) info on one dino.
+
+// GET /dinosaurs/edit/:id -- READ (show) form for editing one dino.
+
+// PUT /dinosaurs/:id -- UPDATE (edit) info on one dino. -- redirect to /dinosaurs/:id OR /dinosaurs
+
+// DELETE /dinosaurs/:id -- DELETE specified dino.
 
 app.listen(PORT, () => {
     rowdyResults.print()
