d5d28ea2fd428dc8aa7469df1455dbccd0e9a804
BeckyBoo88 <rbooco07@gmail.com>
2021-05-28 15:46:15 -0600

    Doing more stuff I don't understand yet

--- index.js
+++ index.js
@@ -2,16 +2,56 @@
 const express = require('express')
 // rowdy logger for loggin our routes
 const rowdy = require('rowdy-logger')
+const fs = require('fs')
 
 // config app
 const app = express()
 const rowdyResults = rowdy.begin(app)
 const PORT = 3000
+app.use(express.urlencoded({ extended: false}))
 
 // define routes
 app.get('/', (req, res) => {
     res.json({ msg: 'hello dinos'})
 })
+
+// GET /dinosaurs -- READ all dinos
+app.get('/dinosaurs', (req, res) => {
+    // read the dino file
+    const dinosaurs = fs.readFileSync('./dinosaurs.json')
+    const dinoData = JSON.parse(dinosaurs)
+    console.log(dinoData)
+    // send back the json
+    res.json({ dinoData })
+})
+
+// POST /dinosaurs -- CREATE a new dino -- redirect to /dinosaurs
+app.post('/dinosaurs', (req, res) => {
+    // read dino file
+    const dinosaurs = fs.readFileSync('./dinosaurs.json')
+    const dinoData = JSON.parse(dinosaurs)
+
+    console.log(req.body)
+    //add data from the request body to the dino data
+    dinoData.push(req.body)
+
+    // write the file
+    fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinoData))
+
+    // redirect to /dinosaurs
+    res.redirect('/dinosaurs')
+})
+
+
+// GET /dinosaurs/new -- READ (show) a form to add a dino
+
+// GET /dinosaurs/:id -- READ one specific dino
+
+// GET /dinosaurs/edit/:id -- READ (show) form to edit one dino
+
+// PUT /dinosaurs/:id -- UPDATE (edit) one dino -- redirect to /dinosaur/:id
+
+// DELETE /dinosaurs/:id -- DESTROY one specific dino
 
 //listen on a port
 app.listen(PORT, () => {
