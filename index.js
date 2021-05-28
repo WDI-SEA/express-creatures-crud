70035dc67153d2582d23a7f07ddcb88ee1c6d581
miknayr <teamry4n@gmail.com>
2021-05-28 15:47:20 -0700

    full CRUD cycle

--- index.js
+++ index.js
@@ -1,48 +1,105 @@
 // required packages
 const express = require('express')
+// rowdy logger for loggin our routes
 const rowdy = require('rowdy-logger')
 const fs = require('fs')
+
 // config app
 const app = express()
 const rowdyResults = rowdy.begin(app)
 const PORT = 3000
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 
 // define routes
-app.get('/', (req,res) => {
-    res.json({ msg: 'hello dinos!🦖'})
+app.get('/', (req, res) => {
+  res.json({ msg: 'hello dinos! 🦕' })
 })
 
 // GET /dinosaurs -- READ all dinos
 app.get('/dinosaurs', (req, res) => {
-    // read the dino file.
+  // read the dino file
+  const dinosaurs = fs.readFileSync('./dinosaurs.json')
+  const dinoData = JSON.parse(dinosaurs)
+  console.log(dinoData)
+  // send back the json
+  res.json({ dinoData })
+})
+
+// POST /dinosaurs -- CREATE a new dino -- redirect to /dinosaurs
+app.post('/dinosaurs', (req, res) => {
+  // read dino file
+  const dinosaurs = fs.readFileSync('./dinosaurs.json')
+  const dinoData = JSON.parse(dinosaurs)
+
+  console.log(req.body)
+  // add data from the request body to the dino data
+  dinoData.push(req.body)
+
+  // write the file
+  fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinoData))
+
+  // redirect to /dinosaurs
+  res.redirect('/dinosaurs')
+})
+
+// GET /dinosaurs/new -- READ (show) a form to add a dino
+app.get('/dinosaurs/new', (req, res) => {
+  res.json({ msg: 'show form to add a dino' })
+}) 
+
+// GET /dinosaurs/:id -- READ one specific dino
+app.get('/dinosaurs/:id', (req, res) => {
+  // get our dino data
+  const dinosaurs = fs.readFileSync('./dinosaurs.json')
+  const dinoData = JSON.parse(dinosaurs)
+
+  // look up one dino with the request parameters
+  const dino = dinoData[req.params.id]
+
+  // send one dino back
+  res.json({ dino })
+})
+
+// GET /dinosaurs/edit/:id -- READ (show) form to edit one dino
+app.get('/dinosaurs/edit/:id', (req, res) => {
+  res.json({ msg: `show form to edit dino ${req.params.id}` })
+})
+
+// PUT /dinosaurs/:id -- UPDATE (edit) one dino -- redirect to /dinosaur/:id
+app.put('/dinosaurs/:id', (req, res) => {
+    // get the dino data from our json
     const dinosaurs = fs.readFileSync('./dinosaurs.json')
     const dinoData = JSON.parse(dinosaurs)
-    // console.log(dinoData)
-    // send back the json.
-    res.json({dinoData})
+    // find on dino from the req.params.id and use the req body to update
+    dinoData[req.params.id].name = req.body.name
+    dinoData[req.params.id].type = req.body.type
 
+    // write the json file
+    fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinoData))
+
+
+    // redirect to /dinosaurs
+    res.redirect('/dinosaurs')
 
 })
-// POST /dinosaurs -- CREATE a new dino -- redirect to /dinosaurs
-app.post ('/dinosaurs', (req, res) => {
-    // read dino file
+
+// DELETE /dinosaur/:id -- DESTROY one specific dino
+app.delete('/dinosaurs/:id', (req, res) => {
+
+    // get out dino json 
     const dinosaurs = fs.readFileSync('./dinosaurs.json')
     const dinoData = JSON.parse(dinosaurs)
 
-    // console.log(req.body)
-    // add data from the request body to the dino data
-    dinoData.push(req.body)
-    // write the file
+    // remove one dino from the array -- use req. params
+    dinoData.splice(req.params.id, 1)
+
+    // save dionsaurs.json
     fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinoData))
+
     // redirect to /dinosaurs
     res.redirect('/dinosaurs')
 })
-// GET /dinosaurs/new -- READ (show) a form to add a dino
-// GET /dinosaurs/:id -- READ one specific dino
-// GET /dinosaurs/edit/:id -- READ show form to edit one dino
-// PUT /dinosaurs/:id -- UPDATE (edit) one specific dino - redirect to /dinosaurs
-// DELETE /dinosaurs/:id -- DESTROY one specific dino
+
 
 //open the port for app to be listening on
 app.listen(PORT, () => {
