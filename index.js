9520a509929e31c54f23df53df12e9ea95bc6d43
Tlamatina <Tassi.lamatina@gmail.com>
2021-05-28 15:38:29 -0700

    dinos added

--- index.js
+++ index.js
@@ -42,15 +42,46 @@ app.post('/dinosaurs', (req, res) => {
     res.redirect('/dinosaurs')
 })
 
-
 // GET /dinosaurs/new -- READ (show) a form to add a dino
+app.get('/dinosaurs/new', (req, res) => {
+    res.json({ msg: 'show form to add dino' })
+})
 
 // GET /dinosaurs/:id -- READ one specific dino
+app.get('/dinosaurs/:id', (req, res) => {
+        // get our dinoData 
+const dinosaurs = fs.readFileSync('./dinosaurs.json')
+const dinoData = JSON.parse(dinosaurs)
+
+        // lookup one dino with the request parameters
+const dino = dinoData[req.params.id]
+
+        // send one dino back
+res.json({ dino })
+})
 
 // GET /dinosaurs/edit/:id -- READ (show) form to edit one dino
+app.get('/dinosaurs/edit', (req, res) => {
+    res.json({ msg: `show form to edit dino ${req.params.id} `})
+})
 
 // PUT /dinosaurs/:id -- UPDATE (edit) one dino -- redirect to /dinosaur/
+app.put('/dinosaurs/:id', (req, res) => {
+    // get the dino data from json
+const dinosaurs = fs.readFileSync('./dinosaurs.json')
+const dinoData = JSON.parse(dinosaurs)
 
+    //find on dino from the req.params.id and us the req body to update
+dinoData[req.params.id].name = req.body.name
+dinoData[req.params.id].type = req.body.type
+
+// write the json file
+fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinoData))
+
+// redirect to /dinosaurs
+res.redirect('/dinosaurs')
+
+})
 // DELETE /dinosaur/:id -- DESTROY one specific dino
 
 //listen on a port
