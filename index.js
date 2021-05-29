9ed7fa6d5f3d1ebf7c0170788485366617b0a08a
lizlukasiewicz <lizlukasiewicz@yahoo.com>
2021-05-28 20:31:37 -0400

    post codealong edits

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const express = require('express')
 const rowdy = require('rowdy-logger')
 const fs = require('fs')
+const { emitWarning } = require('process')
+const layouts = require('express-ejs-layouts')
+const methodOverride = require('method-override')
 
 //CONFIG APP
 const app = express()
 const rowdyResults = rowdy.begin(app)
 const PORT = 3000
+app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false })) //for POST route
+app.use(express.static(__dirname + '/public')) //where CSS will live
+app.use(layouts) //use ejs layouts
+app.use(methodOverride('_method'))
 
 //DEFINE ROUTES
 app.get('/', (req, res) => {
-    res.json({ msg: 'hello dinos!🦖'})
+    res.render('home')
 })
 
 // GET /dinosaurs -- READ all dinos
@@ -20,7 +27,7 @@ app.get('/dinosaurs', (req, res) => {
     const dinoData = JSON.parse(dinosaurs)
     console.log(dinoData)
     //send back the json
-    res.json({ dinoData })
+    res.render('dinosaurs/index.ejs', { dinoData }) //{dinoData} is the same as {dinoData:dinoData}
 })
 
 // POST /dinosaurs -- CREATE a new dino
@@ -41,7 +48,7 @@ app.post('/dinosaurs', (req, res) => {
 
 // GET /dinosaurs/new -- READ (show) a form to add a dino
 app.get('/dinosaurs/new', (req, res) => {
-    res.json({ msg: 'show form to add a dino'})
+    res.render('dinosaurs/new.ejs')
 })
 
 // GET /dinosaurs/:id -- READ one specific dino
@@ -57,12 +64,45 @@ app.get('/dinosaurs/:id', (req, res) => {
     res.json({ dino })
 })
 
-// GET /dinosaurs/edit/:id -- READ (show) form to edit one dino
+// GET /dinosaurs/edit/:id -- READ (show) form to edit one dino -- to DISPLAY a FORM
+app.get('/dinosaurs/edit/:id', (req, res) =>{ // /edit/:id is cuz theres a dino/:id above
+    const dinosaurs = fs.readFileSync('./dinosaurs.json')
+    const dinoData = JSON.parse(dinosaurs)
+
+    const dino = dinoData[req.params.id]
+    // render the template
+    res.render('dinosaurs/edit.ejs', { dino: dino,  dinoId: req.params.id })
+})
 
 // PUT /dinosaurs/:id -- UPDATE (edit) one dinp --- redirect to /dinosaur/:id
+app.put('/dinosaurs/:id', (req, res) => {     //app.put works like app.post
+    //get the dino data from our json
+    const dinosaurs = fs.readFileSync('./dinosaurs.json')
+    const dinoData = JSON.parse(dinosaurs)
+    //find on dino from the req.params.id and use the req body to update
+    dinoData[req.params.id].name = req.body.name
+    dinoData[req.params.id].type = req.body.type
+
+    //write the json file
+    fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinoData))
+    //redirect to /dinosaurs
+    res.redirect('/dinosaurs')
+})
 
 // DELETE /dinosaur/:id -- DESTROY one specific dino
+app.delete('/dinosaurs/:id', (req, res) => {
+    //get our dino JSON
+    const dinosaurs = fs.readFileSync('./dinosaurs.json')
+    const dinoData = JSON.parse(dinosaurs)
 
+    //remove one dino from the array -- use req.params
+    dinoData.splice(req.params.id, 1)
+
+    //save dinosaurs.json
+    fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinoData))
+    //redirect to /dinosaurs
+    res.redirect('/dinosaurs')
+})
 //listen on a port
 app.listen(PORT, () => {
     rowdyResults.print()
